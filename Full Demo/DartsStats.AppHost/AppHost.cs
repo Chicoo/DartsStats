504b7868bc240ea46fe7d6e53b6f65346744a6fa
Marco de Brouwer <marco.debrouwer@devops.ncia.nato.int>
2025-11-03 18:33:30 +0100

    Added Publish

--- Full Demo/DartsStats.AppHost/AppHost.cs
+++ Full Demo/DartsStats.AppHost/AppHost.cs
@@ -2,45 +2,81 @@ using Aspire.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var compose = builder.AddDockerComposeEnvironment("dartsstats-compose");
+compose.Resource.DefaultNetworkName = "darts-stats";
+compose.WithDashboard(configure =>
+{
+    configure.WithHostPort(18888);
+});
+
+var keycloak_username = builder.AddParameter("keycloak-username", "admin");
+var keycloak_password = builder.AddParameter("keycloak-password", "admin");
+if (!int.TryParse(builder.Configuration["Keycloak:Port"], out var keycloak_port))
+{
+    keycloak_port = 8080;
+}
+
 var sqlServer = builder.AddSqlServer("sqlserver")
     .WithLifetime(ContainerLifetime.Persistent)
     .WithDbGate()
     .WithDataVolume()
     .AddDatabase("dartsstats", "DartsStats");
 
-var redis = builder.AddRedis("redis")
+var keycloak = builder.AddKeycloak("keycloak", keycloak_port, keycloak_username, keycloak_password)
+    .WithImageTag("latest")
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithDataVolume()
+    .WithRealmImport("../../data")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerComposeService((resource, service) =>
+    {
+        service.Command =
+        [
+            "start-dev",
+            "--import-realm"
+        ];
+    });
+
+var redis = builder.AddRedis("redis", port: 6380)
     .WithRedisInsight()
     .WithRedisCommander()
     .WithLifetime(ContainerLifetime.Persistent)
     .WithDataVolume();
 
-var keycloak_username = builder.AddParameter("keycloak-username", "admin");
-var keycloak_password = builder.AddParameter("keycloak-password", "admin");
-if(!int.TryParse(builder.Configuration["Keycloak:Port"], out var keycloak_port))
-{
-    keycloak_port = 8080;
-}
-
-var keycloak = builder.AddKeycloak("keycloak", keycloak_port, keycloak_username, keycloak_password)
-    .WithImageTag("latest")
-    .WithLifetime(ContainerLifetime.Persistent)
-    .WithDataVolume()
-    .WithRealmImport("../../data");
-
-// Add the API project
-var api = builder.AddProject<Projects.DartsStats_Api>("dartsStats-api")
+var api = builder.AddProject<Projects.DartsStats_Api>("dartsapi")
     .WaitFor(keycloak)
     .WithEnvironment("Keycloak__Authority", $"{keycloak.GetEndpoint("http")}/realms/dartsstats")
     .WithReference(redis)
     .WaitFor(redis)
     .WaitFor(sqlServer)
-    .WithEnvironment("ConnectionStrings__dartsstats", sqlServer.Resource.ConnectionStringExpression);
+    .WithEnvironment("ConnectionStrings__dartsstats", sqlServer.Resource.ConnectionStringExpression)
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerComposeService((resource, service) =>
+    {
+        service.Ports =
+        [
+            "5167:${DARTSAPI_PORT}"
+        ];
+    });
 
-// Add the React frontend using npm with proper endpoint configuration and API reference
 builder.AddJavaScriptApp("dartsStats-frontend", "../client", "dev")
     .WithReference(api)
+    .WaitFor(api)
     .WithEnvironment("VITE_API_BASE_URL", api.GetEndpoint("http"))
     .WithHttpEndpoint(env: "PORT")
-    .WithExternalHttpEndpoints();
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile(options =>
+    {
+        options.WithDockerfile("../client");
+        options.WithImageTag("latest");
+        options.WithImageRegistry("acrtopfas.azurecr.io");
+    })
+    .PublishAsDockerComposeService((resource, service) =>
+    {
+        service.Ports =
+        [
+            "8000:8000"
+        ];
+    });
 
-await builder.Build().RunAsync();
+builder.Build().Run();
